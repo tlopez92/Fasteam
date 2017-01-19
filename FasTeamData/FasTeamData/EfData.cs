9ac0b8a0f6e02d6f05d341a00fb6a8fd0d8dd2f4
wlopez <wlopez.career@gmail.com>
2017-01-19 09:04:04 -0500

    Created user model and some get methods to pull from database

---
+++ FasTeamData/FasTeamData/EfData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FasTeamData
+{
+  public class EfData
+  {
+    private static FasdsTeamEntities ed = new FasdsTeamEntities();
+    
+    /// <summary>
+    /// Get functions to get users
+    /// states, locations, and roles
+    /// and convert them to a list object
+    /// </summary>
+    /// <returns></returns>
+    public List<User> GetUsers()
+    {
+      return ed.Users.ToList();
+    }
+
+    public List<State> GetStates()
+    {
+      return ed.States.ToList();
+    }
+
+    public List<Location> GetLocations()
+    {
+      return ed.Locations.ToList();
+    }
+
+    public List<Role> GetRoles()
+    {
+      return ed.Roles.ToList();
+    }
+
+
+    /// <summary>
+    /// Finds user by first and last name
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns></returns>
+    public User FindUserByName(User user)
+    {
+      return ed.Users.Where(u => u.FirstName == user.FirstName && u.LastName == user.LastName).ToList().First();
+    }
+  }
+}
