7d3c4ca3569324af26ce8584cb1f761941ca07b2
wlopez <wlopez.career@gmail.com>
2017-01-19 15:50:45 -0500

    created the soap service

---
+++ FasTeamData/FasdsTeamSoap/FasService.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using FasdsTeamSoap.DAO;
+using FasTeamData;
+using FasdsTeamSoap.Mapper;
+
+namespace FasdsTeamSoap
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "FasService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class FasService : IDataContract
+    {
+        private readonly EfData ef = new EfData();
+        private readonly MapperEf mapper = new MapperEf();
+
+        public int AddUser(UserDao user)
+        {
+            return ef.addUser(mapper.MapToData(user));
+        }
+
+        public List<UserDao> GetUsers()
+        {
+            var users = new List<UserDao>();
+            var dataUsers = ef.GetUsers();
+
+            foreach(var item in dataUsers)
+            {
+                users.Add(mapper.MapToService(item));
+            }
+
+            return users;
+        }
+
+        public bool UpdateUser(UserDao user)
+        {
+            return ef.UpdateUser(mapper.MapToData(user));
+        }
+    }
+}
---
+++ FasTeamData/FasdsTeamSoap/IDataContract.cs
@@ -0,0 +1,26 @@
+﻿using FasdsTeamSoap.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FasdsTeamSoap
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IDataContract
+    {
+        [OperationContract]
+        List<UserDao> GetUsers();
+
+
+        [OperationContract]
+        int AddUser(UserDao user);
+
+        [OperationContract]
+        bool UpdateUser(UserDao user);
+    }
+}
