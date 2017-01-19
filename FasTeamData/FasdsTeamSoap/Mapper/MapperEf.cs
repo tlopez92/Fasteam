9b49bd61791e1fd2a4c78f55d7fd667feed67193
wlopez <wlopez.career@gmail.com>
2017-01-19 15:29:11 -0500

    Added automapper

---
+++ FasTeamData/FasdsTeamSoap/Mapper/MapperEf.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using FasdsTeamSoap.DAO;
+using FasTeamData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FasdsTeamSoap.Mapper
+{
+    public class MapperEf
+    {
+        private readonly MapperConfiguration UserMapper = new MapperConfiguration(u => u.CreateMap<User, UserDao>());
+
+        private readonly MapperConfiguration UserMapperReverse = new MapperConfiguration(u => u.CreateMap<UserDao, User>());
+
+        public UserDao MapToService(User user)
+        {
+            var mapper = UserMapper.CreateMapper();
+            return mapper.Map<UserDao>(user);
+        }
+
+        public User MapToData(UserDao user)
+        {
+            var mapper = UserMapperReverse.CreateMapper();
+            return mapper.Map<User>(user);
+        }
+    }
+}
