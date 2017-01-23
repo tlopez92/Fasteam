4171e546a1c19e911d5845d03d544522870e31e4
wlopez <wlopez.career@gmail.com>
2017-01-23 14:19:22 -0500

    Created new solution with a few projects inside.

---
+++ FasTeamLogic/FasTeamLogic/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace FasTeamLogic.Controllers
+{
+    public class UserController : ApiController
+    {
+    }
+}
---
+++ FasTeamLogic/FasTeamLogic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FasTeamLogic.WebApiApplication" Language="C#" %>
