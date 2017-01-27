0c0223fb94ad52c05d4f95d6881d7bcfa7491731
Kevin <kevin.walsh@accenturefederal.com>
2017-01-27 08:44:50 -0500

    Added AngularJS to front-end, includes basic service to be updated

--- FasTeamWebUI/FasTeamWebUI/Scripts/App/Modules/app.js
+++ FasTeamWebUI/FasTeamWebUI/Scripts/App/Modules/app.js
@@ -1,12 +1,13 @@
-﻿(function (ga)
+﻿
+(function (ga)
 {
     'use strict';
 
     ga.angular = angular.module('ahApp', ['ngRoute']);
     ga.angular.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/', {
-            controller: 'userController',
+            controller: 'appCtrl',
             templateUrl: 'modules/User/view.html'
-        })
+        });
     }]);
 })(window.ahApp || (window.ahApp = {}));
